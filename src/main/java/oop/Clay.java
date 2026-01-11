a26a87b23d2eb287d1ce4ff9bc6ba1fb800213ad
bmc2425 <recheriyan@stsaviour.org>
2026-01-10 22:00:16 -0500

    oop

--- src/main/java/oop/Clay.java
+++ src/main/java/oop/Clay.java
@@ -1,11 +1,11 @@
 package oop;
-public class Clay extends Sculpture {
-    public int pieces;
+public class Clay extends Sculpture { // public class that extends painting
+    public int pieces; // variable (clay) related to sculptures
     public Clay(String title, int pieces) {
-        super(title);
+        super(title); // supper used to indicate title and number of layers used in acrylic painting
         this.pieces = pieces;
     }
-    public void addPiece() {
-        this.pieces += 1;
+    public void addPiece() { // adds a piece of clay to the sculpture
+        this.pieces += 1; 
     }
 }
