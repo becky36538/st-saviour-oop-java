70392d9d9d8342d03f06c820dd331757fca06425
bmc2425 <recheriyan@stsaviour.org>
2026-01-10 21:57:03 -0500

    oop

--- src/main/java/oop/Painting.java
+++ src/main/java/oop/Painting.java
@@ -1,10 +1,10 @@
 package oop;
-public abstract class Painting extends Art {
-    public final boolean IS_2D = true;
-    public Painting(String title) {
+public abstract class Painting extends Art { // public class that extends art
+    public final boolean IS_2D = true; // if painting is 2d then it is a painting
+    public Painting(String title) { // passes title to art by using "supper" method
         super(title);
     }
-    public String hang() {
+    public String hang() { // if title belongs to a painting, it can be hung on a wall
         return this.getTitle() + " can be hung on the wall.";
     }
 }
--- src/main/java/oop/Sculpture.java
+++ src/main/java/oop/Sculpture.java
@@ -1,10 +1,10 @@
 package oop;
-public abstract class Sculpture extends Art {
-    public final boolean IS_3D = true;
-    public Sculpture(String title) {
+public abstract class Sculpture extends Art { // public class that extends art
+    public final boolean IS_3D = true; // if painting is 3d then it is a painting
+    public Sculpture(String title) { // passes title to art by using "supper" method
         super(title);
     }
-    public String displayOnPedestal() {
-        return this.getTitle() + " can be hung on the wall.";
+    public String displayOnPedestal() { // if title belongs to a sculpture, it will be displayed on a pedestal
+        return this.getTitle() + " is displayed on a pedestal.";
     }
 }
