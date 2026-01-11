97051090920274a970ea522463785a46bc3b738d
bmc2425 <recheriyan@stsaviour.org>
2026-01-10 21:51:00 -0500

    oop

--- src/main/java/oop/Art.java
+++ src/main/java/oop/Art.java
@@ -1,7 +1,8 @@
 package oop;
-
+// abstract grandparent class
 public abstract class Art {
-    private String title; {
+    private String title;
+    public Art(String title) {
         this.title = title;
     }
     public void setTitle(String title) {
@@ -10,7 +11,7 @@ public abstract class Art {
     public String getTitle() {
         return this.title;
     }
-    public String display() {
+    public String display () {
         return this.title + " is a work of art.";
     }
 }
