e703b035e2977636491e88b7bfda3d85a6aff224
bmc2425 <recheriyan@stsaviour.org>
2026-01-10 21:52:57 -0500

    oop

--- src/main/java/oop/Art.java
+++ src/main/java/oop/Art.java
@@ -1,17 +1,17 @@
 package oop;
 // abstract grandparent class
 public abstract class Art {
-    private String title;
-    public Art(String title) {
+    private String title; //private variable
+    public Art(String title) { // constructor used for subclasses
         this.title = title;
     }
-    public void setTitle(String title) {
+    public void setTitle(String title) { // method that shows how title changes depending on the artwork
         this.title = title;
     }
-    public String getTitle() {
+    public String getTitle() { // method that retrieves the title of artwork
         return this.title;
     }
-    public String display () {
+    public String display () { // says that any title of artwork is a work of art
         return this.title + " is a work of art.";
     }
 }
