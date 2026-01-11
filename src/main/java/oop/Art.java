a59903ba3a5483d4cf99a5410a20676b68ed6441
bmc2425 <recheriyan@stsaviour.org>
2026-01-10 21:02:13 -0500

    oop

--- src/main/java/oop/Art.java
+++ src/main/java/oop/Art.java
@@ -1,9 +1,16 @@
+package oop;
+
 public abstract class Art {
-    private String medium;
-    public Art (String medium) {
-        this.medium = medium;
+    private String title; {
+        this.title = title;
     }
-    public String getMedium() {
-        return medium;
+    public void setTitle(String title) {
+        this.title = title;
     }
+    public String getTitle() {
+        return this.title;
     }
+    public String display() {
+        return this.title + " is a work of art.";
+    }
+}
--- src/main/java/oop/Painting.java
+++
@@ -1,7 +0,0 @@
-public abstract class Painting extends Art {
-    public final boolean ACRYLIC = true;
-
-    public Painting(String medium) {
-        super(medium);
-    }
-    public String ()
